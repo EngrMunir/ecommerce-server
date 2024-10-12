5b08c8f9bd786c85045433d22ad343068c8775c0
Munir <smsirajulmonir@gmail.com>
2024-10-12 15:57:59 -0700

    jwt implemented for security

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const multer=require("multer");
 const path = require("path");
 const cors = require("cors");
 const { type } = require('os');
+const { error } = require('console');
 
 app.use(express.json());
 app.use(cors());
@@ -110,6 +111,85 @@ app.post('/removeproduct',async(req,res)=>{
         success:true,
         name:req.body.name
     })
+})
+
+// creating api for getting all products
+app.get('/allproducts',async(req,res)=>{
+    let products = await Product.find({});
+    console.log("All Products Fetched")
+    res.send(products);
+})
+
+// Schema creating for user model
+const Users = mongoose.model('Users',{
+    name:{
+        type:String,
+    },
+    email:{
+        type:String,
+        unique:true,
+    },
+    password:{
+        type:String,
+    },
+    cartData:{
+        type:Object,
+    },
+    date:{
+        type:Date,
+        default:Date.now,
+    }
+})
+
+// creating endpoint for registering the
+app.post('/signup', async(req,res)=>{
+    let check = await Users.findOne({email:req.body.email});
+    if(check){
+        return res.status(400).json({success:false, errors:"existing user already exist"})
+    }
+    let cart = {};
+    for(let i =0; i<300; i++){
+        cart[i]=0;
+    }
+    const user = new Users({
+        name:req.body.username,
+        email:req.body.email,
+        password:req.body.password,
+        cartData:cart,
+
+    })
+    await user.save();
+
+    const data ={
+        user:{
+            id:user.id
+        }
+    }
+    const token = jwt.sign(data,'secret');
+    res.json({success:true,token})
+})
+
+// creating endpoint for user login
+app.post('/login', async(req,res)=>{
+    let user = await Users.findOne({email:req.body.email});
+    if(user){
+        const passCompare = req.body.password === user.password;
+        if(passCompare){
+            const data = {
+                user:{
+                    id:user.id
+                }
+            }
+            const token = jwt.sign(data,'secret_economics');
+            res.json({success:true, token});
+        }
+        else{
+            res.json({success:false, errors:"Wrong Password"});
+        }
+    }
+    else{
+        res.json({success:false, errors:"Wrong Email Id"})
+    }
 })
 app.listen(port,(error)=>{
     if(!error){
